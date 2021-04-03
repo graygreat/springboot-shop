a2e46d98dc9a896a8b2fc740177125c8023843ce
oopsys6 <operasystem6@naver.com>
2021-04-03 23:07:23 +0900

    refactor: @XtoOne 관계 fetch = LAZY 추가

--- src/main/java/shop/springbootshop/domain/Address.java
+++ src/main/java/shop/springbootshop/domain/Address.java
@@ -1,15 +1,24 @@
 package shop.springbootshop.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
