89246320c4de3742384f7b15e3cbff4624e50cd5
oopsys6 <operasystem6@naver.com>
2021-04-07 03:03:31 +0900

    refactor: @RequiredArgsConstructor 어노테이션 추가

--- src/main/java/shop/springbootshop/domain/MemberRepository.java
+++ src/main/java/shop/springbootshop/domain/MemberRepository.java
@@ -1,5 +1,6 @@
 package shop.springbootshop.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -7,10 +8,10 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
