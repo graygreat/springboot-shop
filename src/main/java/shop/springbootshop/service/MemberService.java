2ee9b00487ba3936cc9c17724c6b4f3c1352d0f5
oopsys6 <operasystem6@naver.com>
2021-04-07 03:03:50 +0900

    feat: MemberService 구현

---
+++ src/main/java/shop/springbootshop/service/MemberService.java
@@ -0,0 +1,42 @@
+package shop.springbootshop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.springbootshop.domain.Member;
+import shop.springbootshop.domain.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);    // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByName(member.getName());
+        Optional<Member> optionalMember = Optional.ofNullable(findMember);
+        if (optionalMember.isPresent()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
