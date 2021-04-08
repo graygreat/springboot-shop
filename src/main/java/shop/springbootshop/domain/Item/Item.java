61bffdc82142dd08367a3edade93971286aa3b91
oopsys6 <operasystem6@naver.com>
2021-04-09 00:00:24 +0900

    feat: 재고 추가, 삭제 구현 및 예외처리 발생 상황 구현

--- src/main/java/shop/springbootshop/domain/Item/Item.java
+++ src/main/java/shop/springbootshop/domain/Item/Item.java
@@ -3,6 +3,7 @@ package shop.springbootshop.domain.Item;
 import lombok.Getter;
 import lombok.Setter;
 import shop.springbootshop.domain.Category;
+import shop.springbootshop.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -25,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 없습니다.");
+        }
+        this.stockQuantity = restStock;
+    }
 }
